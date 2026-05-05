4a5a5b6ee9f0f1702989429ded27853ff523e5cc
martial.polliotto <martial.polliotto@gmail.com>
2026-05-05 08:51:14 +0200

    feat(auth): integration de CodeIgniter Shield
    - composer require codeigniter4/shield (et codeigniter4/settings en transit)
    - spark shield:setup : creation Auth.php, AuthGroups.php, AuthToken.php, mises a jour Routes/Security/Autoload
    - spark migrate --all : tables users, auth_identities, auth_logins, auth_remember_tokens, auth_token_logins, auth_groups_users, auth_permissions_users, settings
    - routes /register, /login, /logout, /login/magic-link operationnelles (HTTP 200)
    - Email::fromEmail = no-reply@cyberrun.fr (requis par Shield setup)

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'no-reply@cyberrun.fr';
+    public string $fromName   = 'CyberRun';
     public string $recipients = '';
 
     /**
